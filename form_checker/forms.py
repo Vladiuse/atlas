7f39b01624f0ee183d1d46f3281dd4e6d216a966
vladiuse <vladiuse@gmail.com>
2025-05-29 20:20:58 +0300

    add field html to checkForm form

--- form_checker/forms.py
+++ form_checker/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class CheckFormsByUrlForm(forms.Form):
-    url = forms.URLField()
+    url = forms.URLField(required=False)
+    html = forms.CharField(required=False)
+
+    def clean(self) -> dict:
+        cleaned_data = super().clean()
+        url = cleaned_data.get('url')
+        html = cleaned_data.get('html')
+        if not any([url, html]):
+            raise ValidationError('Должно быть заполнено одно из полей')
+        return cleaned_data
