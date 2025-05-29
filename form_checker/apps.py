7b8bd2e7ff591bfb2eb1167c1c1d553ecc24dabf
vladiuse <vladiuse@gmail.com>
2025-05-29 19:54:54 +0300

    add apps

--- form_checker/apps.py
+++ form_checker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FormChekerConfig(AppConfig):
+class FormCheckerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "form_cheker"
+    name = "form_checker"
