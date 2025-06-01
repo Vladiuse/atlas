e1d1170b687e34f0c98fd6e5c148dfdf91fd9571
vladiuse <vladiuse@gmail.com>
2025-06-01 16:10:48 +0300

    fix login in attr validations

--- html_checker/tag_attribut.py
+++ html_checker/tag_attribut.py
@@ -45,13 +45,13 @@ class HtmlTagAttribute:
                 self.required_validation()
             except ValidationError as error:
                 self.errors.append(error)
-            else:
-                # check only if attribute exist
-                try:
-                    self.expected_validation()
-                    self.choices_validation()
-                except ValidationError as error:
-                    self.errors.append(error)
+
+        if self.value is not None:
+            try:
+                self.expected_validation()
+                self.choices_validation()
+            except ValidationError as error:
+                self.errors.append(error)
         try:
             self.validate()
         except ValidationError as error:
