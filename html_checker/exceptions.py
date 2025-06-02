035d8818ac110f6723b34de943bb5be3858c1412
vladiuse <vladiuse@gmail.com>
2025-06-02 22:14:47 +0300

    add __repr__ method to ValidationError remove dataclass from ValidationError

--- html_checker/exceptions.py
+++ html_checker/exceptions.py
@@ -1,15 +1,22 @@
-from dataclasses import dataclass
-
 from common.exceptions import AppException
 
-from .constants import ERROR
+from .constants import ERROR, ErrorLevel
 
 
 class FormNotFound(AppException):
     pass
 
 
-@dataclass
 class ValidationError(Exception):
-    message: str
-    level: str = ERROR
+    def __init__(self, message: str, level: ErrorLevel = ERROR):
+        self.message = message
+        self.level = level
+
+    def __repr__(self):
+        return f"ValidationError(message='{self.message}', level={self.level}"
+
+    def to_detail(self) -> dict:
+        return {
+            "message": str(self.message),
+            "level": str(self.level),
+        }
