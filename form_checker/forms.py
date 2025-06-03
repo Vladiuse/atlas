a28b857fd73d0533ab03a1d5d1ab907bced92619
vladiuse <vladiuse@gmail.com>
2025-06-03 14:47:14 +0300

    изменил поле выбора пресета

--- form_checker/forms.py
+++ form_checker/forms.py
@@ -1,19 +1,22 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from common.constants import TEAMS_CHOICES, ATLAS_TEAM
+
+from form_checker.form_checker.presets import PRESETS_MAP
+
+choices = [(preset_name, preset_name) for preset_name in PRESETS_MAP]
 
 
 class CheckFormsByUrlForm(forms.Form):
-    team = forms.ChoiceField(choices=TEAMS_CHOICES, initial=(ATLAS_TEAM,ATLAS_TEAM))
+    preset_name = forms.ChoiceField(choices=choices)
     url = forms.URLField(required=False)
     html = forms.CharField(required=False)
 
     def clean(self) -> dict:
         cleaned_data = super().clean()
-        url = cleaned_data.get('url')
-        html = cleaned_data.get('html')
+        url = cleaned_data.get("url")
+        html = cleaned_data.get("html")
         if not any([url, html]):
-            raise ValidationError('Должно быть заполнено одно из полей')
+            raise ValidationError("Должно быть заполнено одно из полей")
         if all([url, html]):
-            raise ValidationError('2 поля заполнять нельзя')
+            raise ValidationError("2 поля заполнять нельзя")
         return cleaned_data
