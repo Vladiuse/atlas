4b892e2136230d243c65671c6abab1041ef601c3
vladiuse <vladiuse@gmail.com>
2025-06-03 16:02:21 +0300

    сделал PRESETS_MAP OrderedDict для указания порядка

--- form_checker/form_checker/presets/__init__.py
+++ form_checker/form_checker/presets/__init__.py
@@ -1,5 +1,11 @@
+from collections import OrderedDict
+
+from .atlas import AtlasHtml
 from .test import TestHtml
 
-PRESETS_MAP = {
-    'TEST':TestHtml,
-}
+PRESETS_MAP = OrderedDict(
+    [
+        ("Atlas", AtlasHtml),
+        ("TEST", TestHtml),
+    ],
+)
