85633e6e3dad8d46db5d331a5615f69f15dcd865
vladiuse <vladiuse@gmail.com>
2025-06-03 18:42:19 +0300

    добавил проверку на наличие функций js в коде index.html

--- form_checker/form_checker/presets/atlas.py
+++ form_checker/form_checker/presets/atlas.py
@@ -1,6 +1,6 @@
+import re
 from html_checker import HtmlTagAttribute, TagChecker, levels
 from html_checker.exceptions import ValidationError
-
 
 class Title(TagChecker):
     def validate(self) -> None:
@@ -26,8 +26,8 @@ class Sub23Input(TagChecker):
     autocomplete = HtmlTagAttribute(expected="postal-code")
 
 
-class Sub9Input(TagChecker):
-    SELECTOR = "input[name=sub_id_9]"
+class Sub9Select(TagChecker):
+    SELECTOR = "select[name=sub_id_9]"
     DEFAULT_ERROR_LEVEL = levels.INFO
     autocomplete = HtmlTagAttribute(expected="address-level1")
 
@@ -63,9 +63,38 @@ class OrderForm(TagChecker):
     sub_id_22 = Sub22Input()
     sub_id_23 = Sub23Input()
 
-    sub_id_9 = Sub9Input()
+    sub_id_9 = Sub9Select()
+
+
+class JsFuncDetector(TagChecker):
+    """Чекер для поиска наличия js функции к коде index.html"""
+    FUNCTION_NAME_TO_SEARCH = None
+
+    def validate(self) -> None:
+        if self.FUNCTION_NAME_TO_SEARCH is None:
+            raise AttributeError('Set FUNCTION_NAME_TO_SEARCH')
+        scripts = self.root.elem.select("script")
+        is_func_exist = False
+        for script in scripts:
+            pattern = rf"function[\t ]+{re.escape(self.FUNCTION_NAME_TO_SEARCH)}[\t ]*\("
+            if re.search(pattern, script.text):
+                is_func_exist = True
+                break
+        if not is_func_exist:
+            raise ValidationError(f"Функция {self.FUNCTION_NAME_TO_SEARCH} не найдена", level=levels.WARNING)
+
+
+class GetShortImageSrcDetector(JsFuncDetector):
+    FUNCTION_NAME_TO_SEARCH = "getShortImageSrc"
+    SELECTOR = "script-getShortImageSrc"
+
+class InjectScriptDetector(JsFuncDetector):
+    FUNCTION_NAME_TO_SEARCH = "injectScript"
+    SELECTOR = "script-injectScript"
 
 
 class AtlasHtml(TagChecker):
     title = Title(selector="title")
     form = OrderForm(selector="form", many=True)
+    short_img_script = GetShortImageSrcDetector(required=False)
+    inject_script = InjectScriptDetector(required=False)
